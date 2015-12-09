ff45d7008299d703d29e20af07a585b693f4a26d
Robinson Greig <robinsongreig@gmail.com>
2015-12-09 11:16:26 -0500

    added html structure to the layout file, removed html structure from the home page file.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ferris | Your personal guide to your city."
+    assert_select "title", "Ferris | Your Personal Guide to the City"
   end
 
   test "should get terms" do
