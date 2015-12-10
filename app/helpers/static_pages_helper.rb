877033d41f46afa2084cf76bdafb2ec81a93d2bc
Robinson Greig <robinsongreig@gmail.com>
2015-12-09 19:49:34 -0500

    reinstalled semantic-ui-sass fingers crossed it works

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,19 @@
 module StaticPagesHelper
 end
+
+class ApplicationController
+  semantic_breadcrumb :index, :root_path
+end
+
+class ExamplesController < ApplicationController
+  semantic_breadcrumb :index, :examples_path
+
+  def index
+  end
+
+  def show
+    @example = Example.find params[:id]
+    semantic_breadcrumb @example.name, example_path(@example)
+    # semantic_breadcrumb :show, example_path(@example)
+  end
+end
