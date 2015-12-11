41ad61b9a16cbc0fee2c4d02defd367b136f7546
Robinson Greig <robinsongreig@gmail.com>
2015-12-11 18:13:58 -0500

    finish event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,24 @@
 class EventsController < ApplicationController
-  def new
-  end
-
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title, :description)
+    end
 end
