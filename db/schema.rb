9b28a10d99fe370b8f7ca068a3980181f4e83d65
Robinson Greig <robinsongreig@gmail.com>
2015-12-14 13:57:44 -0500

    add neighborhood to to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214143918) do
+ActiveRecord::Schema.define(version: 20151213140022) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -23,13 +23,13 @@ ActiveRecord::Schema.define(version: 20151214143918) do
     t.string   "website"
     t.integer  "price"
     t.string   "purchase_url"
+    t.string   "neighborhood"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "neighborhood"
   end
 
 end
