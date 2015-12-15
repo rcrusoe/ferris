c333e6993eba5a620690d23cc6ab5b0abefa9340
Robinson Greig <robinsongreig@gmail.com>
2015-12-15 11:54:07 -0500

    remove silly page title test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ferris | Your Personal Guide to the City"
   end
 
 end
