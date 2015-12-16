2f280f4212bff504ff99d91798b4f05ee2831614
Robinson Greig <robinsongreig@gmail.com>
2015-12-16 13:49:35 -0500

    one last blurb fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -74,7 +74,7 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:title, :description, :date, :start_time, :end_time, :address, :neighborhood, :website, :price, :purchase_url, :image)
+    params.require(:event).permit(:title, :description, :date, :start_time, :end_time, :address, :neighborhood, :website, :price, :purchase_url, :image, :short_blurb)
   end
 
 end
