26d306fde90c982060a5665b96aa9d13c051d2c0
Robinson Greig <robinsongreig@gmail.com>
2015-12-16 14:05:13 -0500

    add case insensitive search

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,6 +8,6 @@ class Event < ActiveRecord::Base
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 	def self.search(search)
-	  where("title || description || date || neighborhood LIKE ?", "%#{search}%")
+	  where("title || description || date || neighborhood ILIKE ?", "%#{search}%")
 	end
 end
