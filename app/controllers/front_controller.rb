5f7b7e094442f85437e8be47ed6dc47ebe3d52cf
Paul Berry <pdbthefifth@gmail.com>
2016-03-03 13:25:26 -0500

    added handle and timestamp

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -2,7 +2,8 @@ class FrontController < ApplicationController
 
   # Any time new message is sent from Front, webhook passes event data to this endpoint
   def mirror
-    ap "NUMBER: #{params['message']['conversation']['recipient']}"
+    ap "NUMBER: #{params['message']['conversation']['recipient']['handle']}"
+    ap "DATE: #{Time.at(params['message']['date']/1000)}"
     ap "INBOUND: #{params['message']['inbound']}"
     ap "TYPE: #{params['message']['type']}"
     ap "TEXT: #{params['message']['text']}"
