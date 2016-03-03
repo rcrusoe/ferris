0ae3812f5e2b1ec2807dc86ad82f65e9f2f893f4
Paul Berry <pdbthefifth@gmail.com>
2016-03-03 13:12:52 -0500

    individual parameter output for testing webhook

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -2,6 +2,9 @@ class FrontController < ApplicationController
 
   # Any time new message is sent from Front, webhook passes event data to this endpoint
   def mirror
-    ap params.to_json
+    ap "NUMBER: #{params['message']['conversation']['recipient']}"
+    ap "INBOUND: #{params['message']['inbound']}"
+    ap "TYPE: #{params['message']['type']}"
+    ap "TEXT: #{params['message']['text']}"
   end
 end
