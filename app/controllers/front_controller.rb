612527a55b9b2df185be580ff4a85bacc8c431ea
Paul Berry <pdbthefifth@gmail.com>
2016-03-03 12:02:13 -0500

    better formatting of webhook data

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -2,7 +2,7 @@ class FrontController < ApplicationController
 
   # Any time new message is sent from Front, webhook passes event data to this endpoint
   def mirror
-    logger.debug 'PARAMS: ' + params
-    ap params
+    data = JSON.parse(params)
+    ap data
   end
 end
