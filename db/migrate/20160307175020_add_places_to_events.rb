bb216e2e512e4c4d2cc65716b98be096e18e30b0
Paul Berry <pdbthefifth@gmail.com>
2016-03-07 13:49:31 -0500

    allow places_id to be null

--- db/migrate/20160307175020_add_places_to_events.rb
+++ db/migrate/20160307175020_add_places_to_events.rb
@@ -1,5 +1,5 @@
 class AddPlacesToEvents < ActiveRecord::Migration
   def change
-    add_reference :events, :place, index: true, foreign_key: true
+    add_reference :events, :place, index: true, foreign_key: true, null: true
   end
 end
