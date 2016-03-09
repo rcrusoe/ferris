d7040cda41f01bd233879d7b0531e72f61e232ca
Paul Berry <pdbthefifth@gmail.com>
2016-03-09 15:33:38 -0500

    fixing S3 bucket credentials to not use foreman for now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,13 +39,14 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # TODO: replace with .env file and foreman
   #s3 configuration
   config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-          :bucket => ENV['S3_BUCKET_NAME'],
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+          :bucket => 'ferris-production',
+          :access_key_id => 'AKIAIJ2CHFO5U45WNUDA',
+          :secret_access_key => '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'
       }
   }
 end
