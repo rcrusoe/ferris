228b3b4c8927f5334199ea5247291b6c3c2ce4f4
Paul Berry <pdbthefifth@gmail.com>
2016-03-22 12:59:14 -0400

    rec test v2 -- multiple recommendations and personal rec option

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,23 +1,41 @@
 var URL;
+var index;
 var PagesController = Paloma.controller('Pages');
 
 PagesController.prototype.rec = function() {
   URL = this.params['URL'];
+  index = this.params['index'];
 
   // Change button styling on click.
   $(".recommend-button").click(function(e){
     e.preventDefault();
     var cb = generate_callback($(this));
     var id = $(this).attr('id');
-    mixpanel.track("Recommendation Clicked", {
-      "event": id
+    var eventName = $.trim($('.header'+'#'+id).text());
+    mixpanel.track("Recommendation Clicked -Test", {
+      "request": index,
+      "event": eventName
+    }, setTimeout(cb, 500));
+  });
+
+  $("#customRec").click(function(e){
+    e.preventDefault();
+    var cb = generate_callback($(this));
+    var recText = $('#custom_rec_text').val();
+    mixpanel.track("Personal Recommendation Clicked -Test", {
+      "request": index,
+      "text": recText
     }, setTimeout(cb, 500));
   });
 
   function generate_callback(a) {
     return function() {
       a.find('button').toggleClass("basic");
-      $(location).attr('href', URL+'/another');
+      if (index < 4) {
+        $(location).attr('href', URL+'/rec?index='+(index+1));
+      }else {
+        $(location).attr('href', URL+'/another');
+      }
     }
   }
 };
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,22 @@
 class PagesController < ApplicationController
-  def home
-  end
-
+  # quick and dirty CI recommendation test (NEVER WRITE FRONT END CODE LIKE THIS OUTSIDE TESTING)
   def rec
-    js :URL => request.base_url
+    names = ['Allison', 'Joe', 'Jack', 'Bob', 'Fred']
+    messages = ['I like food', 'I like music', 'Movies are fun', 'Need a date night', 'Alcohol and Breasts']
+    @events = Event.limit(3).order('RANDOM()')
+
+    if params[:index]
+      index = params[:index].to_i
+    else
+      index = 0
+    end
+
+    @name = names[index]
+    @message = messages[index]
+    # @event1 = events[0]
+    # @event2 = events[1]
+    # @event3 = events[2]
+
+    js :URL => request.base_url, :index => index
   end
 end
